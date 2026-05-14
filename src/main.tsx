0a74b47ea7a311fb9a7e58eaf88bce009682fa1d
enter-pro <github@enter.pro>
2026-05-14 18:23:07 +0000

    feat(analytics): integrate Enter Analytics SDK

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
+import { bootstrapEnterAnalytics } from '@enter-pro/analytics-sdk';
 import App from './App.tsx';
 import './index.css';
+
+bootstrapEnterAnalytics();
 
 createRoot(document.getElementById('root')!).render(<App />);
